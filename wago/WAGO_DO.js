d546b00d1310c1c0b81bbad7d3567b2e1847e8fb
Chris <aidasan@hotmail.fr>
2019-09-05 16:47:49 +0900

    v0.1.4 test 1

--- wago/WAGO_DO.js
+++ wago/WAGO_DO.js
@@ -2,12 +2,14 @@ module.exports = function(RED) {
     "use strict";
 
     function digitalOutput(n) {
-       RED.nodes.createNode(this,n);
-       var node = this;
-       var bitSize = parseInt(n.inputs);
-       var bitOffset = n.offset;
+		RED.nodes.createNode(this,n);
+        var context = this.context();
+		var node = this;
+//		var bitSize = parseInt(n.inputs);
+		var bitOffset = n.offset;
+		var o = [];
 
-        this.on('output', function(msg) {
+		this.on('input', function(msg) {
 			
 			// initialise the context.data to 0 if it doesn't exist already
 			context.set('data') = context.get('data') || 0;
@@ -21,26 +23,29 @@ module.exports = function(RED) {
 				number = (number & clearMask) | (bitValue << bitPosition);
 				// store the context value back
 				context.set('data') = number;
-				return {payload:number};
+				number = number << bitOffset;
+				o = payload:number;
+				node.send(o);
 			} else {
 				node.error("invalid msg topic (must be number between 0 and 15): " + msg.topic);
 			}
 
-			
-			
-			
-            var p = msg.payload >>> bitOffset;
-            var m = [];
-            var o = [];
-            for (var i=0; i<16; i++) {
-              // test top bit and set corresponding payload
-                m[i] = {payload: ((p & 0x8000) ? true : false) };
-                p = p << 1; // divide by two and keep as an integer
-                }
-                o = m.reverse();
-            node.status({fill: "green",shape: "ring",text: bitSize});
-            node.send(o);
-        });
-    }
-    RED.nodes.registerType("Digital Input",digitalInput);
+//			
+//			
+//			
+//			var p = msg.payload >>> bitOffset;
+//			var m = [];
+//			var o = [];
+//			for (var i=0; i<16; i++) {
+//			  // test top bit and set corresponding payload
+//				m[i] = {payload: ((p & 0x8000) ? true : false) };
+//				p = p << 1; // divide by two and keep as an integer
+//				}
+//				o = m.reverse();
+//			node.status({fill: "green",shape: "ring",text: bitSize});
+//			node.send(o);
+		});
+		node.status({fill: "green",shape: "ring"});
+	}
+	RED.nodes.registerType("Digital Output",digitalOutput);
 };
