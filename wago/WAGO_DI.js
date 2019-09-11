80909a6c3739db442d5d850b5a88c0c70ccfb6f4
Chris <aidasan@hotmail.fr>
2019-09-12 00:21:44 +0900

    v0.1.4 test 3
    Improvement on the DI and DO

--- wago/WAGO_DI.js
+++ wago/WAGO_DI.js
@@ -5,22 +5,43 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this,n);
 		var node = this;
 		var bitSize = parseInt(n.outputs);
-		var bitOffset = n.offset;
+		var wordOffset = parseInt(n.wordOffset);
+		var bitOffset = parseInt(n.bitOffset);
 		var name = n.name;
 		var topic = n.topic;
 
 		this.on('input', function(msg) {
-			var _data = msg.payload >>> bitOffset;
 			var _object = [];
-			var _bit = 0;
-			var _mask = 0x0001 << (bitSize - 1);
-			for (var i = 0; i < bitSize; i++) {
-				// calculate the _bit number
-				_bit = bitSize - (i + 1);
-				// Test the _data & _mask and set corresponding payload
-				_object[_bit] = {topic:(topic + "." + _bit),payload:((_data & _mask) ? true : false)};
-				// divide by two and keep _data as an integer
-				_data = _data << 1;
+			// Test is we have an array as input
+			if (msg.payload.constructor === Array) {
+				// Test if the array is an array of boolean
+				if (msg.payload[0].constructor === Boolean) {
+					for (var i = 0; i < bitSize; i++) {
+						// Copy the input payload in the right output payload (with the offset)
+						_object[i] = {topic:(topic + "." + i),payload:(msg.payload[bitOffset + i] ? true : false)};
+					}
+				} else if (msg.payload[0].constructor === Number) {
+					var _wordOffset = wordOffset;
+					var _mask = 0x0001 << bitOffset;
+					for (var i = 0; i < bitSize; i++) {
+						if ((_mask > 0x8000) || (_mask === 0x0000)) {
+							_wordOffset = _wordOffset + 1;
+							_mask = 0x0001;
+						}
+						// Test the input payload & _mask and set corresponding payload
+						_object[i] = {topic:(topic + "." + i),payload:((msg.payload[_wordOffset] & _mask) ? true : false)};
+						// shift the mask to select the next bit
+						_mask = _mask << 1;
+					}
+				}
+			} else {
+				var _mask = 0x0001 << bitOffset;
+				for (var i = 0; i < bitSize; i++) {
+					// Test the input payload & _mask and set corresponding payload
+					_object[i] = {topic:(topic + "." + i),payload:((msg.payload & _mask) ? true : false)};
+					// shift the mask to select the next bit
+					_mask = _mask << 1;
+				}
 			}
 			//o = m.reverse();
 			node.send(_object);
